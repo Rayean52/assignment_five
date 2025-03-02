f4c61db0e3779bcac18fa0e9f81712857317877e
Abdullah Rayean <rayeanabdullah@gmail.com>
2025-03-02 13:06:57 +0600

    javascript functionality added

---
+++ script/main.js
@@ -0,0 +1,50 @@
+function updateClock() {
+    let now = new Date();
+    let timeString = now.toLocaleTimeString();
+    let clockElement = document.getElementById('clock');
+    clockElement.textContent = timeString;
+}
+setInterval(updateClock, 1000);
+updateClock();
+
+const buttons = document.getElementsByClassName('card-btn');
+
+for (const btn of buttons) {
+    btn.addEventListener('click', function () {
+        alert("Board Updated successfully");
+
+        let taskDecrement = document.getElementById('task-decrement').innerText;
+        let convertedTaskDecrement = parseInt(taskDecrement);
+        let decrementResult = convertedTaskDecrement - 1;
+        document.getElementById('task-decrement').innerText = decrementResult;
+
+        let taskIncrement = document.getElementById('task-increment').innerText;
+        let convertedTaskIncrement = parseInt(taskIncrement);
+        let incrementResult = convertedTaskIncrement + 1;
+        document.getElementById('task-increment').innerText = incrementResult;
+
+        let newActivity = document.createElement('p');
+        newActivity.innerText = "You have completed the task at 12:25 pm";
+        newActivity.classList.add('bg-gray-200', 'text-sm', 'p-2', 'rounded-lg')
+        document.getElementById('add-activity').appendChild(newActivity);
+
+
+        btn.setAttribute('disabled', true);
+
+
+    })
+}
+
+document.getElementById('clear-history')
+    .addEventListener('click', function () {
+        document.getElementById('add-activity').innerHTML = "";
+    })
+
+document.getElementById('btn-theme').addEventListener('click', function(){
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.body.style.background = color
+})
